c11809ed9441f5fb183f54f46d58820a89aa114b
Chris Granger <ibdknox@gmail.com>
2011-10-05 11:36:12 -0700

    initial commit
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject korma "1.0.0-SNAPSHOT"
+  :description "A SQL solution for Clojure"
+  :dependencies [[org.clojure/clojure "1.3.0"]])
---
+++ test/korma/test/core.clj
@@ -0,0 +1,66 @@
+(ns korma.test.core
+  (:use [korma.core])
+  (:use [clojure.test]))
+
+(defentity user)
+
+(deftest select-function
+         (is (= (-> (select-query "user")
+                  (fields :id :username)
+                  (where {:username "chris"})
+                  (order :created)
+                  (limit 5)
+                  (offset 3)
+                  (as-sql))
+                "SELECT id, username FROM user WHERE username = 'chris' ORDER BY created DESC LIMIT 5 OFFSET 3")))
+
+(deftest simple-selects
+         (sql-only
+           (let [results ["SELECT * FROM user"
+                          "SELECT id, username FROM user"
+                          "SELECT * FROM user WHERE email = 'hey@hey.com' AND username = 'chris'"
+                          "SELECT * FROM user WHERE username = 'chris' ORDER BY created DESC"
+                          "SELECT * FROM user WHERE username = 'chris' ORDER BY created DESC LIMIT 5 OFFSET 3"]
+                 queries [(select user)
+                          (select user
+                                  (fields :id :username))
+                          (select user
+                                  (where {:username "chris"
+                                          :email "hey@hey.com"}))
+                          (select user 
+                                  (where {:username "chris"})
+                                  (order :created))
+                          (select user 
+                                  (where {:username "chris"})
+                                  (order :created)
+                                  (limit 5)
+                                  (offset 3))]]
+             (doseq [[r q] (map vector results queries)]
+               (is (= q r))))))
+
+(deftest update-function
+         (is (= (-> (update-query "user")
+                  (fields {:first "chris"
+                           :last "granger"})
+                  (where {:id 3})
+                  (as-sql))
+                "UPDATE user SET last = 'granger', first = 'chris' WHERE id = '3'")))
+
+(deftest update-queries
+         (sql-only
+           (let [results ["UPDATE user SET first = 'chris'"
+                          "UPDATE user SET first = 'chris' WHERE id = '3'"
+                          "UPDATE user SET last = 'granger', first = 'chris' WHERE id = '3'"]
+                 queries [(update user
+                                  (fields {:first "chris"}))
+                          (update user
+                                  (fields {:first "chris"})
+                                  (where {:id 3}))
+                          (update user
+                                  (fields {:first "chris"
+                                           :last "granger"})
+                                  (where {:id 3}))]]
+             (doseq [[r q] (map vector results queries)]
+               (is (= q r))))))
+
+
