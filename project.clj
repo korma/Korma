da9415f9db589f2d30e4d485bdff3da851cc23e9
Chris Granger <ibdknox@gmail.com>
2011-11-02 14:21:39 -0700

    remove postgres
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject korma "0.2.0"
   :description "Delicious SQL for Clojure"
   :dependencies [[org.clojure/clojure "[1.2.1],[1.3.0]"]
-                 [postgresql "8.4-701.jdbc4"]
                  [c3p0/c3p0 "0.9.1.2"]
                            [log4j "1.2.15" :exclusions [javax.mail/mail
                                                         javax.jms/jms
