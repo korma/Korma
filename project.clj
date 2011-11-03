e5f74046c7da0249a95071d42a9bc676884b0072
Chris Granger <ibdknox@gmail.com>
2011-11-02 22:08:15 -0700

    inc minor
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject korma "0.2.0"
+(defproject korma "0.2.1"
             :description "Delicious SQL for Clojure"
             :dependencies [[org.clojure/clojure "[1.2.1],[1.3.0]"]
                            [c3p0/c3p0 "0.9.1.2"]
