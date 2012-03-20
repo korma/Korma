33daabfe3af8228126990adf8842460bc1067455
Chris Granger <ibdknox@gmail.com>
2012-03-20 09:36:46 -0700

    inc to beta8
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject korma "0.3.0-beta7"
+(defproject korma "0.3.0-beta8"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/ibdknox/korma"
   :dependencies [[org.clojure/clojure "1.3.0"]
