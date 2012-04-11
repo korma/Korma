9186db9b0a950154149c4edc8da295fc5cfc3971
Chris Granger <ibdknox@gmail.com>
2012-04-11 15:33:40 -0700

    move to the latest java.jdbc inc to beta10
    Signed-off-by: Chris Granger <ibdknox@gmail.com>

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject korma "0.3.0-beta9"
+(defproject korma "0.3.0-beta10"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/ibdknox/korma"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [c3p0/c3p0 "0.9.1.2"]
-                 [org.clojure/java.jdbc "0.1.0"]]
+                 [org.clojure/java.jdbc "0.1.3"]]
   :codox {:exclude [korma.sql.engine korma.sql.fns korma.sql.utils]}
           :dev-dependencies [[postgresql "9.0-801.jdbc4"]])
