1059e920f7276d211d8fa1a02cb2f1893e8ab7dc
Ben Burkert <ben@benburkert.com>
2012-10-22 13:26:37 -0700

    Fix transform function calls.

--- src/korma/core.clj
+++ src/korma/core.clj
@@ -353,7 +353,7 @@
     results
     (if-let [trans (seq (-> query :ent :transforms))]
       (let [trans-fn (apply comp trans)]
-        (map trans-fn results))
+        (trans-fn results))
       results)))
 
 (defn- apply-prepares
