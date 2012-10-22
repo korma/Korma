a07d280586e727d7eab564ccee187505990e2925
Ben Burkert <ben@benburkert.com>
2012-10-22 14:19:19 -0700

    Return the same type of transformed results.
    The results may be a vector or a map, but the transformed type should be
    consistent.

--- src/korma/core.clj
+++ src/korma/core.clj
@@ -353,7 +353,7 @@
     results
     (if-let [trans (seq (-> query :ent :transforms))]
       (let [trans-fn (apply comp trans)]
-        (trans-fn results))
+        (if (vector? results) (map trans-fn results) (trans-fn results)))
       results)))
 
 (defn- apply-prepares
