7f4a613c53c231e1da62dedd548401c169d1d89d
Alex Baranosky <alexander.baranosky@gmail.com>
2013-01-01 16:48:08 -0800

    inc version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject korma "0.3.0-beta12"
+(defproject korma "0.3.0-beta13"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/ibdknox/korma"
   :dependencies [[org.clojure/clojure "1.3.0"]
