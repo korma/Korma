ac333f94eb7bd7fc379420434e125c098cfb4234
David Kettering <david.ringman@gmail.com>
2013-02-10 13:28:37 -0600

    Set LF as line separator
    Edited project file to tell JVM to use unix-style line endings.
    This lets 'lein test' succeed on a fresh korma clone in Windows.

--- project.clj
+++ project.clj
@@ -24,6 +24,7 @@
              :1.5.0 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}}
   :aliases {"run-tests" ["with-profile" "1.3.0:1.4.0:1.5.0" "test"]
             "slamhound" ["run" "-m" "slam.hound"]}
+  :jvm-opts ["-Dline.separator=\n"]
 
   ;; Lein1 - will be removed at some point
   :dependencies [[org.clojure/clojure "1.3.0"]
