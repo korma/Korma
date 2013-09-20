e7e57052b01c5e1970d9d616d3d183e85e7324cc
Takashi AOKI <federkasten@me.com>
2013-09-20 12:45:42 +0900

    add test on checkin and idle connection test period option

--- src/korma/db.clj
+++ src/korma/db.clj
@@ -17,12 +17,17 @@
   "Create a connection pool for the given database spec."
   [{:keys [subprotocol subname classname user password
            excess-timeout idle-timeout minimum-pool-size maximum-pool-size
-           test-connection-query test-connection-on-checkout]
+           test-connection-query
+           idle-connection-test-period
+           test-connection-on-checkin
+           test-connection-on-checkout]
     :or {excess-timeout (* 30 60)
          idle-timeout (* 3 60 60)
          minimum-pool-size 3
          maximum-pool-size 15
          test-connection-query nil
+         idle-connection-test-period 0
+         test-connection-on-checkin false
          test-connection-on-checkout false}
     :as spec}]
   {:datasource (doto (ComboPooledDataSource.)
@@ -34,6 +39,8 @@
                  (.setMaxIdleTime idle-timeout)
                  (.setMinPoolSize minimum-pool-size)
                  (.setMaxPoolSize maximum-pool-size)
+                 (.setIdleConnectionTestPeriod idle-connection-test-period)
+                 (.setTestConnectionOnCheckin test-connection-on-checkin)
                  (.setTestConnectionOnCheckout test-connection-on-checkout)
                  (.setPreferredTestQuery test-connection-query))})
 
