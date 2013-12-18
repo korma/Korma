bb73e9c42c0897a6a974e6e31a63f0e8607bc23a
Josh Tilles <MerelyAPseudonym@gmail.com>
2013-12-18 10:34:05 -0500

    Extract common JDBC-connector dependencies.
    Even if you run `lein with-profile 1.3 test`, the `:test` profile (and
    its associated dependencies!) are merged into the project map, so we
    don't need to worry about creating, e.g., a test profile for Clojure
    1.3, a test profile for Clojure 1.4, a test profile for Clojure 1.5, etc.
    
    Oh, and I'm just gambling that Postgres and Criterium really are meant
    as dev-dependencies and not test-dependencies.

--- project.clj
+++ project.clj
@@ -15,27 +15,18 @@
 
   :min-lein-version "2.0.0"
   
-  :profiles {:dev {:dependencies [[com.h2database/h2 "1.3.164"]
-                                  [gui-diff "0.4.0"]
+  :profiles {:dev {:dependencies [[gui-diff "0.4.0"]
                                   [postgresql "9.0-801.jdbc4"]
                                   [slamhound "1.3.1"]
-                                  [mysql/mysql-connector-java "5.1.22"]
                                   [criterium "0.3.1"]]
                    :plugins [[codox "0.6.4"]
                              [jonase/eastwood "0.0.2"]
                              [lein-localrepo "0.4.1"]]}
-             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]
-                                  [mysql/mysql-connector-java "5.1.22"]
-                                  [com.h2database/h2 "1.3.164"]
-                                  [criterium "0.3.1"]]}
-             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]
-                                  [mysql/mysql-connector-java "5.1.22"]
-                                  [com.h2database/h2 "1.3.164"]
-                                  [criterium "0.3.1"]]}
-             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]
-                                  [mysql/mysql-connector-java "5.1.22"]
-                                  [com.h2database/h2 "1.3.164"]
-                                  [criterium "0.3.1"]]}}
+             :test {:dependencies [[mysql/mysql-connector-java "5.1.22"]
+                                   [com.h2database/h2 "1.3.164"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}}
   :aliases {"run-tests" ["with-profile" "1.3:1.4:1.5" "test"]
             "slamhound" ["run" "-m" "slam.hound"]}
   :jvm-opts ["-Dline.separator=\n"])
