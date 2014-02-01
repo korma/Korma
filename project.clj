7b52eab85b29252597d18ab131e73cc22ee7e672
Josh Tilles <MerelyAPseudonym@gmail.com>
2014-01-31 19:59:51 -0500

    Bump version to 0.3.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject korma "0.3.0"
+(defproject korma "0.3.1-SNAPSHOT"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/korma/Korma"
   :mailing-list {:name "Korma Google Group"
