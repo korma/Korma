dc0cbcd6803c44f7dfcc5acb581ae0d8e14d589a
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-06-09 08:42:56 +0300

    Remove test for functionality that doesn't exist

--- test/korma/test/integration/with_db.clj
+++ test/korma/test/integration/with_db.clj
@@ -1,24 +1,10 @@
 (ns korma.test.integration.with-db
   (:require [clojure.string :as string]
-            [clojure.java.jdbc.deprecated :as jdbc]
-            [criterium.core :as cr]
             [korma.core :as kcore]
             [korma.db :as kdb])
   (:import [java.util.concurrent Executors ThreadPoolExecutor CountDownLatch])
   (:use clojure.test
         korma.test.integration.helpers))
-
-(defmacro with-db-broken [db & body]
-  `(jdbc/with-connection (kdb/get-connection ~db)
-     ~@body))
-
-(deftest fails-with-old-style-with-db
-  (testing "with-db without setting @_default in with-db binding"
-    (kdb/default-connection nil)
-    (with-db-broken (mem-db) (populate 2))
-    (is (thrown? Exception (dorun (with-db-broken (mem-db)
-                                    (with-naming dash-naming-strategy
-                                      (kcore/select user (kcore/with address)))))))))
 
 (deftest with-db-success
   (testing "with-db with setting *current-db* in with-db binding"
