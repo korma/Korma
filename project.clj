3b2f5d3456068469c65f2fcec5f1922222ff7b70
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-08-27 07:39:44 +0300

    Bump version to 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject korma "0.4.0"
+(defproject korma "0.4.1-SNAPSHOT"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/korma/Korma"
   :mailing-list {:name "Korma Google Group"
