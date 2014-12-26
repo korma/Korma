81290bcb2aaaed58c54fee6b8ae3615d03135be1
Immo Heikkinen <immo.heikkinen@gmail.com>
2014-12-26 13:40:04 +0200

    Fix test context string

--- test/korma/test/integration/delete.clj
+++ test/korma/test/integration/delete.clj
@@ -16,5 +16,5 @@
     (is (= 1 (delete address (where {:id 1})))))
   (testing "Deleting multiple rows"
     (is (= 3 (delete address (where {:id [< 5]})))))
-  (testing "No rows are updated"
+  (testing "No rows are deleted"
     (is (= 0 (delete address (where {:id -1}))))))
