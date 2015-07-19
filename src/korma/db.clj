de5c4abb9b7f16beea8f4f9419f489872d12c00f
Immo Heikkinen <immo.heikkinen@gmail.com>
2015-07-19 20:48:47 +0300

    Revert "Functions that generate database specifications generate connection-uri."
    This reverts commit b235facb906fd4e8db6624a5e95621bde4114243.

--- src/korma/db.clj
+++ src/korma/db.clj
@@ -52,7 +52,7 @@
 
 (defn connection-pool
   "Create a connection pool for the given database spec."
-  [{:keys [connection-uri classname
+  [{:keys [subprotocol subname classname
            excess-timeout idle-timeout
            initial-pool-size minimum-pool-size maximum-pool-size
            test-connection-query
@@ -73,9 +73,9 @@
     (throw (Exception. "com.mchange.v2.c3p0.ComboPooledDataSource not found in class path."))) 
   {:datasource (doto (resolve-new ComboPooledDataSource)
                  (.setDriverClass classname)
-                 (.setJdbcUrl connection-uri)
+                 (.setJdbcUrl (str "jdbc:" subprotocol ":" subname))
                  (.setProperties (as-properties (dissoc spec
-                                                        :make-pool? :classname :connection-uri
+                                                        :make-pool? :classname :subprotocol :subname
                                                         :naming :delimiters :alias-delimiter
                                                         :excess-timeout :idle-timeout
                                                         :initial-pool-size :minimum-pool-size :maximum-pool-size
@@ -137,7 +137,8 @@
     :or {host "localhost", port 3050, db "", make-pool? true}
     :as opts}]
   (merge {:classname "org.firebirdsql.jdbc.FBDriver" ; must be in classpath
-          :connection-uri (str "jdbc:firebirdsql:" host "/" port ":" db)
+          :subprotocol "firebirdsql"
+          :subname (str host "/" port ":" db)
           :make-pool? make-pool?
           :encoding "UTF8"}
          (dissoc opts :host :port :db)))
@@ -150,7 +151,8 @@
     :or {host "localhost", port 5432, db "", make-pool? true}
     :as opts}]
   (merge {:classname "org.postgresql.Driver" ; must be in classpath
-          :connection-uri (str "jdbc:postgresql://" host ":" port "/" db)
+          :subprotocol "postgresql"
+          :subname (str "//" host ":" port "/" db)
           :make-pool? make-pool?}
          (dissoc opts :host :port :db)))
 
@@ -161,7 +163,8 @@
     :or {host "localhost", port 1521, make-pool? true}
     :as opts}]
   (merge {:classname "oracle.jdbc.driver.OracleDriver" ; must be in classpath
-          :connection-uri (str "jdbc:oracle:thin:@" host ":" port)
+          :subprotocol "oracle:thin"
+          :subname (str "@" host ":" port)
           :make-pool? make-pool?}
          (dissoc opts :host :port)))
 
@@ -173,7 +176,8 @@
     :or {host "localhost", port 3306, db "", make-pool? true}
     :as opts}]
   (merge {:classname "com.mysql.jdbc.Driver" ; must be in classpath
-          :connection-uri (str "jdbc:mysql://" host ":" port "/" db)
+          :subprotocol "mysql"
+          :subname (str "//" host ":" port "/" db)
           :delimiters "`"
           :make-pool? make-pool?}
          (dissoc opts :host :port :db)))
@@ -186,7 +190,8 @@
     :or {host "localhost", port 5433, db "", make-pool? true}
     :as opts}]
   (merge {:classname "com.vertica.jdbc.Driver" ; must be in classpath
-          :connection-uri (str "jdbc:vertica://" host ":" port "/" db)
+          :subprotocol "vertica"
+          :subname (str "//" host ":" port "/" db)
           :delimiters "\""
           :make-pool? make-pool?}
          (dissoc opts :host :port :db)))
@@ -199,7 +204,8 @@
     :or {user "dbuser", password "dbpassword", db "", host "localhost", port 1433, make-pool? true}
     :as opts}]
   (merge {:classname "com.microsoft.sqlserver.jdbc.SQLServerDriver" ; must be in classpath
-          :connection-uri (str "jdbc:sqlserver://" host ":" port ";database=" db ";user=" user ";password=" password)
+          :subprotocol "sqlserver"
+          :subname (str "//" host ":" port ";database=" db ";user=" user ";password=" password)
           :make-pool? make-pool?}
          (dissoc opts :host :port :db)))
 
@@ -210,9 +216,10 @@
     :or {db "", make-pool? false}
     :as opts}]
   (merge {:classname "sun.jdbc.odbc.JdbcOdbcDriver" ; must be in classpath
-          :connection-uri (str "jdbc:odbc:" (str "Driver={Microsoft Access Driver (*.mdb"
-                                      (when (.endsWith db ".accdb") ", *.accdb")
-                                      ")};Dbq=" db))
+          :subprotocol "odbc"
+          :subname (str "Driver={Microsoft Access Driver (*.mdb"
+                        (when (.endsWith db ".accdb") ", *.accdb")
+                        ")};Dbq=" db)
           :make-pool? make-pool?}
          (dissoc opts :db)))
 
@@ -223,7 +230,8 @@
     :or {dsn "", make-pool? true}
     :as opts}]
   (merge {:classname "sun.jdbc.odbc.JdbcOdbcDriver" ; must be in classpath
-          :connection-uri (str "jdbc:odbc:" dsn)
+          :subprotocol "odbc"
+          :subname dsn
           :make-pool? make-pool?}
          (dissoc opts :dsn)))
 
@@ -234,7 +242,8 @@
     :or {db "sqlite.db", make-pool? true}
     :as opts}]
   (merge {:classname "org.sqlite.JDBC" ; must be in classpath
-          :connection-uri (str "jdbc:sqlite:" db)
+          :subprotocol "sqlite"
+          :subname db
           :make-pool? make-pool?}
          (dissoc opts :db)))
 
@@ -245,7 +254,8 @@
     :or {db "h2.db", make-pool? true}
     :as opts}]
   (merge {:classname "org.h2.Driver" ; must be in classpath
-          :connection-uri (str "jdbc:h2:" db)
+          :subprotocol "h2"
+          :subname db
           :make-pool? make-pool?}
          (dissoc opts :db)))
 
--- test/korma/test/db.clj
+++ test/korma/test/db.clj
@@ -14,13 +14,15 @@
 
 (def db-config-with-defaults
   {:classname "org.h2.Driver"
-   :connection-uri "jdbc:h2:mem:db_connectivity_test_db"
+   :subprotocol "h2"
+   :subname "mem:db_connectivity_test_db"
    :user "bob"
    :password "password"})
 
 (def db-config-with-options-set
   {:classname "org.h2.Driver"
-   :connection-uri "jdbc:h2:mem:db_connectivity_test_db"
+   :subprotocol "h2"
+   :subname "mem:db_connectivity_test_db"
    :excess-timeout 99
    :idle-timeout 88
    :initial-pool-size 10
@@ -87,13 +89,15 @@
 (deftest test-firebird
   (testing "firebirdsql - defaults"
     (is (= {:classname "org.firebirdsql.jdbc.FBDriver"
-            :connection-uri "jdbc:firebirdsql:localhost/3050:"
+            :subprotocol "firebirdsql"
+            :subname "localhost/3050:"
             :make-pool? true
             :encoding "UTF8"}
            (firebird {}))))
   (testing "firebirdsql - options selected"
     (is (= {:classname "org.firebirdsql.jdbc.FBDriver"
-            :connection-uri "jdbc:firebirdsql:host/port:db"
+            :subprotocol "firebirdsql"
+            :subname "host/port:db"
             :make-pool? false
             :encoding "NONE"}
            (firebird {:host "host"
@@ -105,12 +109,14 @@
 (deftest test-postgres
   (testing "postgres - defaults"
     (is (= {:classname "org.postgresql.Driver"
-            :connection-uri "jdbc:postgresql://localhost:5432/"
+            :subprotocol "postgresql"
+            :subname "//localhost:5432/"
             :make-pool? true}
            (postgres {}))))
   (testing "postgres - options selected"
     (is (= {:classname "org.postgresql.Driver"
-            :connection-uri "jdbc:postgresql://host:port/db"
+            :subprotocol "postgresql"
+            :subname "//host:port/db"
             :make-pool? false}
            (postgres {:host "host"
                       :port "port"
@@ -120,12 +126,14 @@
 (deftest test-oracle
   (testing "oracle - defaults"
     (is (= {:classname "oracle.jdbc.driver.OracleDriver"
-            :connection-uri "jdbc:oracle:thin:@localhost:1521"
+            :subprotocol "oracle:thin"
+            :subname "@localhost:1521"
             :make-pool? true}
            (oracle {}))))
   (testing "oracle - options selected"
     (is (= {:classname "oracle.jdbc.driver.OracleDriver"
-            :connection-uri "jdbc:oracle:thin:@host:port"
+            :subprotocol "oracle:thin"
+            :subname "@host:port"
             :make-pool? false}
            (oracle {:host "host"
                     :port "port"
@@ -134,13 +142,15 @@
 (deftest test-mysql
   (testing "mysql - defaults"
     (is (= {:classname "com.mysql.jdbc.Driver"
-            :connection-uri "jdbc:mysql://localhost:3306/"
+            :subprotocol "mysql"
+            :subname "//localhost:3306/"
             :delimiters "`"
             :make-pool? true}
            (mysql {}))))
   (testing "mysql - options selected"
     (is (= {:classname "com.mysql.jdbc.Driver"
-            :connection-uri "jdbc:mysql://host:port/db"
+            :subprotocol "mysql"
+            :subname "//host:port/db"
             :delimiters "`"
             :make-pool? false}
            (mysql {:host "host"
@@ -151,13 +161,15 @@
 (deftest test-vertica
   (testing "vertica - defaults"
     (is (= {:classname "com.vertica.jdbc.Driver"
-            :connection-uri "jdbc:vertica://localhost:5433/"
+            :subprotocol "vertica"
+            :subname "//localhost:5433/"
             :delimiters "\""
             :make-pool? true}
            (vertica {}))))
   (testing "vertica - options selected"
     (is (= {:classname "com.vertica.jdbc.Driver"
-            :connection-uri "jdbc:vertica://host:port/db"
+            :subprotocol "vertica"
+            :subname "//host:port/db"
             :delimiters "\""
             :make-pool? false}
            (vertica {:host "host"
@@ -168,14 +180,16 @@
 (deftest test-mssql
   (testing "mssql - defaults"
     (is (= {:classname "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-            :connection-uri "jdbc:sqlserver://localhost:1433;database=;user=dbuser;password=dbpassword"
+            :subprotocol "sqlserver"
+            :subname "//localhost:1433;database=;user=dbuser;password=dbpassword"
             :make-pool? true}
            (mssql {}))))
   (testing "mssql - options selected"
     (is (= {:password "password"
             :user "user"
             :classname "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-            :connection-uri "jdbc:sqlserver://host:port;database=db;user=user;password=password"
+            :subprotocol "sqlserver"
+            :subname "//host:port;database=db;user=user;password=password"
             :make-pool? false}
            (mssql {:host "host"
                    :port "port"
@@ -187,17 +201,20 @@
 (deftest test-msaccess
   (testing "msaccess - defaults"
     (is (= {:classname "sun.jdbc.odbc.JdbcOdbcDriver"
-            :connection-uri "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};Dbq="
+            :subprotocol "odbc"
+            :subname "Driver={Microsoft Access Driver (*.mdb)};Dbq="
             :make-pool? false}
            (msaccess {}))))
   (testing "msaccess - .mdb selected"
     (is (= {:classname "sun.jdbc.odbc.JdbcOdbcDriver"
-            :connection-uri "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb)};Dbq=db.mdb"
+            :subprotocol "odbc"
+            :subname "Driver={Microsoft Access Driver (*.mdb)};Dbq=db.mdb"
             :make-pool? true}
            (msaccess {:db "db.mdb" :make-pool? true}))))
   (testing "msaccess - .accdb selected"
     (is (= {:classname "sun.jdbc.odbc.JdbcOdbcDriver"
-            :connection-uri (str "jdbc:odbc:Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
+            :subprotocol "odbc"
+            :subname (str "Driver={Microsoft Access Driver (*.mdb, *.accdb)};"
                           "Dbq=db.accdb")
             :make-pool? true}
            (msaccess {:db "db.accdb" :make-pool? true})))))
@@ -205,36 +222,42 @@
 (deftest test-odbc
   (testing "odbc - defaults"
     (is (= {:classname "sun.jdbc.odbc.JdbcOdbcDriver"
-            :connection-uri "jdbc:odbc:"
+            :subprotocol "odbc"
+            :subname ""
             :make-pool? true}
            (odbc {}))))
   (testing "odbc - options selected"
     (is (= {:classname "sun.jdbc.odbc.JdbcOdbcDriver"
-            :connection-uri "jdbc:odbc:MyDsn"
+            :subprotocol "odbc"
+            :subname "MyDsn"
             :make-pool? false}
            (odbc {:dsn "MyDsn" :make-pool? false})))))
 
 (deftest test-sqlite3
   (testing "sqlite3 - defaults"
     (is (= {:classname "org.sqlite.JDBC"
-            :connection-uri "jdbc:sqlite:sqlite.db"
+            :subprotocol "sqlite"
+            :subname "sqlite.db"
             :make-pool? true}
            (sqlite3 {}))))
   (testing "sqlite3 - options selected"
     (is (= {:classname "org.sqlite.JDBC"
-            :connection-uri "jdbc:sqlite:db"
+            :subprotocol "sqlite"
+            :subname "db"
             :make-pool? false}
            (sqlite3 {:db "db" :make-pool? false})))))
 
 (deftest test-h2
   (testing "h2 - defaults"
     (is (= {:classname "org.h2.Driver"
-            :connection-uri "jdbc:h2:h2.db"
+            :subprotocol "h2"
+            :subname "h2.db"
             :make-pool? true}
            (h2 {}))))
   (testing "h2 - options selected"
     (is (= {:classname "org.h2.Driver"
-            :connection-uri "jdbc:h2:db"
+            :subprotocol "h2"
+            :subname "db"
             :make-pool? false}
            (h2 {:db "db" :make-pool? false})))))
 
