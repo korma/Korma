afca04dff11461d3086665556b59d215d02f1a69
Immo Heikkinen <immo.heikkinen@gmail.com>
2016-01-20 07:57:00 +0200

    Clojure 1.8

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
           :src-dir-uri "https://github.com/korma/Korma/blob/v0.4.2/"
           :src-linenum-anchor-prefix "L"}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [c3p0/c3p0 "0.9.1.2"]
                  [org.clojure/java.jdbc "0.3.7"]]
 
@@ -29,7 +29,8 @@
              :1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
-             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}}
-  :aliases {"run-tests" ["with-profile" "1.3:1.4:1.5:1.6:1.7" "test"]
+             :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
+  :aliases {"run-tests" ["with-profile" "1.3:1.4:1.5:1.6:1.7:1.8" "test"]
             "slamhound" ["run" "-m" "slam.hound"]}
   :jvm-opts ["-Dline.separator=\n"])
