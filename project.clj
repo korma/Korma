8692b16df8d1be44d60a49d00bf624e4a09ed563
Marcela Paíno <marcepaino@gmail.com>
2016-09-13 17:10:52 -0300

    Add license to project definition

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject korma "0.5.0-SNAPSHOT"
   :description "Tasty SQL for Clojure"
   :url "http://github.com/korma/Korma"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :mailing-list {:name "Korma Google Group"
                  :subscribe "https://groups.google.com/group/sqlkorma"}
   :codox {:exclude [korma.sql.engine
