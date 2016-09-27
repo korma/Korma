223bd5d6f3126e0add5cdf53cb2d1508f166c258
Immo Heikkinen <immo.heikkinen@gmail.com>
2016-09-27 09:46:03 +0300

    Test against Clojure 1.9

--- project.clj
+++ project.clj
@@ -31,7 +31,8 @@
              :1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}}
-  :aliases {"run-tests" ["with-profile" "1.3:1.4:1.5:1.6:1.7:1.8" "test"]
+             :1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :1.9 {:dependencies [[org.clojure/clojure "1.9.0-alpha13"]]}}
+  :aliases {"run-tests" ["with-profile" "1.4:1.5:1.6:1.7:1.8:1.9" "test"]
             "slamhound" ["run" "-m" "slam.hound"]}
   :jvm-opts ["-Dline.separator=\n"])
